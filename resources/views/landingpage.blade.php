d92175a553bda43a4c04c0ba52b70d93d3e621f3
soulqan <soultan.agnar@gmail.com>
2025-05-08 20:37:13 +0700

    contoh implementasi

---
+++ resources/views/landingpage.blade.php
@@ -0,0 +1,15 @@
+<!-- resources/views/landingpage.blade.php -->
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>landingpage</title>
+
+    @vite('resources/css/landingpage.css')
+
+</head>
+<body>
+
+</body>
+</html>
