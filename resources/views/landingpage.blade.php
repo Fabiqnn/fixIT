9cf6a614d0619b45446c75922777a0bf6ac1fbf7
Fabiqnn <arema20056@gmail.com>
2025-05-09 21:01:21 +0700

    header trial

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,15 +1,1 @@
-<!-- resources/views/landingpage.blade.php -->
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>landingpage</title>
-
-    @vite('resources/css/landingpage.css')
-
-</head>
-<body>
-
-</body>
-</html>
+@extends('layouts_user.template')
---
+++ resources/views/layouts_user/header.blade.php
@@ -0,0 +1,15 @@
+<nav class="bg-yellow-50 flex h-14 p-5 ">
+    <div class="logo flex items-center gap-0.5">
+        <img src="{{ asset('assets/mechanic-hijau-tua.png')}}" alt="logo" class="h-10 w-auto object-contain">
+        <a href="" class="font-monsterrat">fixIT!</a>
+    </div>
+    <ul class="flex">
+        <li><a href="">Beranda</a></li>
+        <li><a href="">Menu</a></li>
+        <li><a href="">Panduan</a></li>
+        <li><a href="">Tentang Kami</a></li>
+    </ul>
+    
+    
+    
+</nav>
