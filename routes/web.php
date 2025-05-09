bb4c3a99f1237ead44d48ca35661d8dc7140d25b
RoyW12 <roywijaya893@gmail.com>
2025-05-09 11:03:22 +0700

    .

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AdminController;
 
 
-// Route::get('/', function () {
-//     return view('landingpage');
-// });
-Route::get('/', [AdminController::class, 'index']);
+Route::get('/', function () {
+    return view('landingpage');
+});
+Route::get('/admin', [AdminController::class, 'index']);
