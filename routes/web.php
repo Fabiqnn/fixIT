94159cf16ebd9cd821b16c05187a4e5522ba7af9
RoyW12 <roywijaya893@gmail.com>
2025-05-09 10:31:37 +0700

    tes layouting

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminController;
 
-Route::get('/', function () {
-    return view('landingpage');
-});
+
+// Route::get('/', function () {
+//     return view('landingpage');
+// });
+Route::get('/', [AdminController::class, 'index']);
