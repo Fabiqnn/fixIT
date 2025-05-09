038f0ee26de785a5fb1c8ee75ba21a251f3773a3
RoyW12 <roywijaya893@gmail.com>
2025-05-09 10:51:29 +0700

    tes ganti logo

--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -5,12 +5,12 @@
         <a href="./index.html" class="brand-link">
             <!--begin::Brand Image-->
             {{-- <img src="../../dist/assets/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image opacity-75 shadow" /> --}}
-            <img src="{{ asset('adminlte/dist/assets/img/AdminLTELogo.png') }}" alt="AdminLTE Logo"
-                class="brand-image opacity-75 shadow" />
+
+            <img src="{{ asset('assets/mechanic.png') }}" alt="Mechanic Logo" class="brand-image opacity-75 shadow" />
 
             <!--end::Brand Image-->
             <!--begin::Brand Text-->
-            <span class="brand-text fw-light">AdminLTE 4</span>
+            <span class="brand-text fw-light">FIXIT!</span>
             <!--end::Brand Text-->
         </a>
         <!--end::Brand Link-->
