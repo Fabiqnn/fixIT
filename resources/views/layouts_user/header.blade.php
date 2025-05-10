a9cc3562f8123921bec5d0d3fa0efb9c6a0d5c58
Fabiqnn <arema20056@gmail.com>
2025-05-10 18:45:53 +0700

    landingpage1

--- resources/views/layouts_user/header.blade.php
+++ resources/views/layouts_user/header.blade.php
@@ -1,4 +1,4 @@
-<nav class="bg-white flex justify-between items-center h-16 px-6">
+<nav class="sticky top-0 bg-white flex justify-between items-center h-16 px-6">
     <!-- Logo Kiri -->
     <div class="logo flex items-center gap-2">
         <img src="{{ asset('assets/mechanic-hijau-tua.png') }}" alt="logo" class="h-12 w-auto object-contain">
@@ -6,13 +6,13 @@
     </div>
 
     <!-- Menu Kanan -->
-    <ul class="flex gap-6 font-medium items-center">
+    <ul class="flex gap-6 font-medium items-center font-inter text-sm">
         <li><a href="#" class="hover:text-D_grey">Beranda</a></li>
         <li><a href="#" class="hover:text-D_grey">Menu</a></li>
         <li><a href="#" class="hover:text-D_grey">Panduan</a></li>
         <li><a href="#" class="hover:text-D_grey">Tentang Kami</a></li>
         <li>
-            <button class="bg-greenPrimary text-white px-4 py-1 rounded hover:bg-greenshade1 transition items-center">
+            <button class="bg-greenPrimary text-white px-4 py-1 rounded hover:bg-greenshade1 transition text-center">
                 Masuk
             </button>
         </li>
